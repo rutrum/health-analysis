ed78929dc034344f5c29b9f13cbd657861bdc691
David Purdum <purdum41@gmail.com>
2022-09-11 14:31:48 -0400

    Update dashboard to use new view

--- src/dashboard.py
+++ src/dashboard.py
@@ -15,49 +15,42 @@ def load_config():
 def date_range(df):
     return dcc.DatePickerRange(
         id="date_picker",
-        min_date_allowed=df["date_formatted"].min(),
-        start_date=df["date_formatted"].min(),
-        max_date_allowed=df["date_formatted"].max(),
-        end_date=df["date_formatted"].max(),
+        min_date_allowed=df["date"].min(),
+        start_date=df["date"].min(),
+        max_date_allowed=df["date"].max(),
+        end_date=df["date"].max(),
     )
 
 def main():
     config = load_config()
 
     conn = sqlite3.connect(config["prepared"]["sqlite"])
-    weight = pd.read_sql("""
-        select *, date(meals.timestamp, 'unixepoch') as date_formatted from weight
-        join (
-            select timestamp, sum(total_posts) as total_posts from meals
-            group by date(timestamp, 'unixepoch')
-        ) as meals
-        where date(meals.timestamp, 'unixepoch') == date(weight.date, 'unixepoch')
-    """, con=conn)
+    daily = pd.read_sql("SELECT * FROM daily", con=conn)
 
     app.layout = html.Div(children=[
-        date_range(weight),
-        dcc.Graph(id="weight_over_time"),
+        date_range(daily),
+        dcc.Graph(id="daily"),
     ])
 
     @app.callback(
-        Output("weight_over_time", "figure"),
+        Output("daily", "figure"),
         Input("date_picker", "start_date"),
         Input("date_picker", "end_date"),
     )
     def update_weight_over_time(start_date, end_date):
-        df = weight
+        df = daily
         if start_date and end_date:
-            df = weight[weight.date_formatted < end_date]
-            df = df[weight.date_formatted > start_date]
+            df = daily[daily.date < end_date]
+            df = df[daily.date > start_date]
 
         fig = make_subplots(specs=[[{"secondary_y": True}]])
 
         fig.add_trace(
-            go.Scatter(x=df["date_formatted"], y=df["weight"], name="Weight")
+            go.Scatter(x=df["date"], y=df["weight"], name="Weight")
         )
 
         fig.add_trace(
-            go.Bar(x=df["date_formatted"], y=df["total_posts"], name="Total Posts"),
+            go.Bar(x=df["date"], y=df["total_posts"], name="Total Posts"),
             secondary_y = True,
         )
 
